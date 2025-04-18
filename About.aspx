354e007bd0810c89757f7ec386e14e8233ae0b50
rybak <rybak@IDEAPAD>
2025-04-18 16:04:13 -0400

    Add project files.

---
+++ About.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="MUTB.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table style="width: 100%;">
+        <tr>
+            <td style="text-align:right">&nbsp;
+                <asp:Label ID="lblDate" Text="" Font-Bold="true" runat="server"></asp:Label>
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td style="align-content:start">&nbsp;
+                <asp:Label ID="lblRefDate" Text="Ref. Date: " Font-Bold="true" runat="server"></asp:Label>&nbsp;&nbsp;
+                <asp:TextBox ID="txtRefDate" TextMode="Date" ReadOnly="false" runat="server"></asp:TextBox>&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="btnLoad" Text="Load" OnClick="btnLoad_Click" width="200px" runat="server" />&nbsp;&nbsp;&nbsp;&nbsp;
+                
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;
+                <asp:GridView ID="GridView1" Visible="true" runat="server"></asp:GridView>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align:left">&nbsp;
+                <asp:Label ID="lblOut" Text="" runat="server"></asp:Label>
+                <br />
+            </td>
+        </tr>
+    </table>
+
+    <br />
+    <br />
+
+</asp:Content>
---
+++ About.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace MUTB
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToString();
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            txtRefDate.Text = "";
+
+            string csvPath = Server.MapPath("~/Data/VT_Balance.csv");
+
+            //Create a DataTable.
+            DataTable dt = new DataTable();
+
+            dt.Columns.AddRange(new DataColumn[12] {
+                new DataColumn("ref_date", typeof(string)), //DateTime
+                new DataColumn("acct_id", typeof(string)), //int
+                new DataColumn("acct_sn", typeof(string)),
+                new DataColumn("ccy_cd", typeof(string)),
+                new DataColumn("l_s_type",typeof(string)),
+                new DataColumn("value_date",typeof(string)), //DateTime
+                new DataColumn("opening_bal",typeof(string)), //decimal
+                new DataColumn("closing_bal",typeof(string)), //decimal
+                new DataColumn("inp_type",typeof(string)), //int
+                new DataColumn("src_type",typeof(string)), //int
+                new DataColumn("upd_userid",typeof(string)),
+                new DataColumn("upd_datetime",typeof(string)) //DateTime
+            });
+
+            //Read the contents of CSV file.
+            string csvData = File.ReadAllText(csvPath);
+
+            bool headerRow = true;
+
+            //Execute a loop over the rows.
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (headerRow == false)
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        dt.Rows.Add();
+                        int i = 0;
+
+                        //Execute a loop over the columns.
+                        foreach (string cell in row.Split(','))
+                        {
+                            dt.Rows[dt.Rows.Count - 1][i] = cell;
+                            i++;
+                        }
+                    }
+                }
+                else
+                {
+                    headerRow = false;
+                }
+            }
+
+            //Bind the DataTable.
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            
+            lblOut.Text = "Loaded at "+ DateTime.Now.ToString();
+        }
+    }
+}
